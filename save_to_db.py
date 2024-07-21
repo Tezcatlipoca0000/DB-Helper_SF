463812cbd97dd400fa14d9c2f805ae62282a5489
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2024-07-21 12:26:55 -0600

    fix: added missing else statement

--- save_to_db.py
+++ save_to_db.py
@@ -1,7 +1,7 @@
 def update_db(dict_prov : dict, exclude : list = [], include : list = [], enforce : bool = False):
 
     '''
-    Function to save FROM a backup_database in an excel file TO PDVDATA.FDB
+    Function to update FROM a backup_database in an excel file (Provedores Todos.xlsx) TO PDV_DB (PDVDATA.FDB)
 
     Parameters:
         - dict_prov: {"Name": ID}
@@ -12,13 +12,11 @@ def update_db(dict_prov : dict, exclude : list = [], include : list = [], enforc
         - exclude: ["Provedor", ...]
             Required: No
             Default: []
-            Is a list of valid names that match those in the dict_prov dictionary. All the names provided will be 
-            excluded from manipulation.
+            Is a list of valid names that match those in the dict_prov dictionary. All the names provided will be excluded from manipulation.
         - include: ["Provedor", ...]
             Required: No
             Default: []
-            Is a list of valid names that match those in the dict_prov dictionary. All the names provided will be
-            the only to be manipulated
+            Is a list of valid names that match those in the dict_prov dictionary. All the names provided will be the only to be manipulated
         - enforce: False | True
             Required: No
             Default: False
@@ -26,10 +24,12 @@ def update_db(dict_prov : dict, exclude : list = [], include : list = [], enforc
             
     '''
 
+    # Imports
     import fdb
     import pandas as pd
     import datetime
 
+    # Initial variables
     excel_df = pd.read_excel('Provedores Todos.xlsm', sheet_name='Datos', index_col=0, na_values='--', skipfooter=10, usecols='A:AE')
     con = fdb.connect(dsn="localhost:C:/Users/casa/Desktop/DB-Helper/PDVDATA.FDB", user='SYSDBA', password='masterkey')
     cur = con.cursor()
@@ -38,8 +38,11 @@ def update_db(dict_prov : dict, exclude : list = [], include : list = [], enforc
     inserted = []
     skipped = []
 
+    # Define function to fix or identify rows in PDV_DB with error
     def fix_row(code, row):
+        # Re-write description to fix error with some characters
         cur.execute( "UPDATE PRODUCTOS SET DESCRIPCION = ? WHERE CODIGO = ?;", (row['Producto'], code) )
+        # Check if error will present in any column 
         cur.execute("SELECT * FROM PRODUCTOS WHERE CODIGO = ?;", (code,))
         try:
             x = cur.fetchone()
@@ -47,68 +50,67 @@ def update_db(dict_prov : dict, exclude : list = [], include : list = [], enforc
             x = 'error'
         return x
 
+    # Define function to update PDV_DB
     def update_or_insert(code, row):
-        #print('Begin operation on code:', code)
 
+        # SELECT row
         cur.execute( "SELECT * FROM PRODUCTOS WHERE CODIGO = ?;", (code,) )
         try:
             x = cur.fetchone()
         except Exception as e:
             x = fix_row(code=code, row=row)
         finally:
-            if x == 'error': # Error detected in DB and NOT fixed
-                #print('Skipping PDV row due to an error that could not be fixed')
+            # Error detected in PDV_DB and NOT fixed
+            if x == 'error': 
                 skipped.append(code)
-            elif not x: # No row found. Insert into DB
-                #print('no data found on DB. Proceding to insert')
+            # No row found. Insert into PDV_DB
+            elif not x: 
                 # define profit
                 profit = float(row['Margen']) * 100 if pd.notna(row['Margen']) else 0
                 #insert
                 cur.execute( "INSERT INTO PRODUCTOS (CHECADO_EN, CODIGO, DESCRIPCION, TVENTA, PCOSTO, PVENTA, DEPT, PROVID, UMEDIDA, MAYOREO, IPRIORIDAD, DINVENTARIO, DINVMINIMO, DINVMAXIMO, PORCENTAJE_GANANCIA, COMPONENTES, IMPUESTOS, PVENTA_ANTERIOR, PCOSTO_ANTERIOR, PMAYOREO_ANTERIOR) VALUES (?, ?, ?, ?, ?, ?, ?, 0, 0, 0.0, 0, 0.0, 0, 0, ?, '', '', 0.0, 0.0, 0.0)", (date, code, row['Producto'], row['TVENTA'], float(row['Costo Unitario']), float(row['Precio al Público']), int(dict_prov[row['Provedor']]), int(profit)) )
                 con.commit()
-                #print('Inserted new row')
                 inserted.append(code)
-            else: # Row found. Update DB
+            # Row found. Update PDV_DB
+            else: 
+                # Update all columns 
                 if enforce:
                     cost = row['Costo Unitario'] if pd.notna(row['Costo Unitario']) else x[3] if x[3] else 0.0
                     old_cost = x[3] if row['Costo Unitario'] != x[3] else x[18] if x[18] else 0.0
                     price = row['Precio al Público'] if pd.notna(row['Precio al Público']) else x[4] if x[4] else 0.0
                     old_price = x[4] if row['Precio al Público'] != x[4] else x[17] if x[17] else 0.0
+                # Update most recent columns
                 else:
                     # Compare dates
                     db_date = x[13] if x[13] else datetime.date(1900, 1, 1)
                     xl_cost_date = pd.Timestamp(row['Última Rev. Costos']).date() if not pd.isnull(row['Última Rev. Costos']) else datetime.date(1900, 1, 1)
                     xl_price_date = pd.Timestamp(row['Última Rev. Precios']).date() if not pd.isnull(row['Última Rev. Precios']) else datetime.date(1900, 1, 1)
-                    # define cost and old_cost
+                    # Define cost and old_cost
                     if xl_cost_date <= db_date:
-                        #print('skiping update of cost in DB')
                         cost = x[3] if x[3] else row['Costo Unitario'] if pd.notna(row['Costo Unitario']) else 0.0
                         old_cost = x[18] if x[18] else 0.0
                     else:
                         cost = row['Costo Unitario'] if pd.notna(row['Costo Unitario']) else x[3] if x[3] else 0.0
                         old_cost = x[3] if x[3] and pd.notna(row['Costo Unitario']) else x[18] if x[18] else 0.0
-                        #print('Going to update cost in DB')
-                    # define price and old_price
+                    # Define price and old_price
                     if xl_price_date <= db_date:
-                        #print('skiping update of price in DB')
                         price = x[4] if x[4] else row['Precio al Público'] if pd.notna(row['Precio al Público']) else 0.0
                         old_price = x[17] if x[17] else 0.0
                     else:
                         price = row['Precio al Público'] if pd.notna(row['Precio al Público']) else x[4] if x[4] else 0.0
                         old_price = x[4] if x[4] and pd.notna(row['Precio al Público']) else x[17] if x[17] else 0.0
-                        #print('Going to update price in DB')
-                # define profit
+                # Define profit
                 try:
                     profit = ((price - cost) / cost) * 100
                 except ZeroDivisionError:
                     profit = 0
-                #update
+                # Update PDV_DB
                 cur.execute( "UPDATE PRODUCTOS SET CHECADO_EN = ?, DESCRIPCION = ?, PCOSTO = ?, PVENTA = ?, DEPT = ?, PORCENTAJE_GANANCIA = ?, PVENTA_ANTERIOR = ?, PCOSTO_ANTERIOR = ? WHERE CODIGO = ?;", (date, row['Producto'], float(cost), float(price), int(dict_prov[row['Provedor']]), int(profit), float(old_price), float(old_cost), code) )
                 con.commit()
-                #print('Updated row')
                 updated.append(code)
                 
 
+    # Iterate over backup_DB and pass code and row to update function
     for idx, row in excel_df.iterrows():
         if include:
             if row['Provedor'] in include:
@@ -129,6 +131,8 @@ def update_db(dict_prov : dict, exclude : list = [], include : list = [], enforc
                         for x in xx:
                             x = x.strip()
                             update_or_insert(code=x, row=row)
+                    else:
+                        update_or_insert(code=x, row=row)
                     
         else:
             if row['Provedor'] in exclude: continue
@@ -144,10 +148,12 @@ def update_db(dict_prov : dict, exclude : list = [], include : list = [], enforc
             if pd.notna(row['Código SuperFuentes']):
                 x = str(row['Código SuperFuentes']).upper()
                 if ',' in x:
-                        xx = x.split(',')
-                        for x in xx:
-                            x = x.strip()
-                            update_or_insert(code=x, row=row)
+                    xx = x.split(',')
+                    for x in xx:
+                        x = x.strip()
+                        update_or_insert(code=x, row=row)
+                else:
+                    update_or_insert(code=x, row=row)
     
 
     con.close()
